6f30e2b78162ac0a200bde710f8ec5c1d4afc5f7
ReedFu(work) <fubingding@163.com>
2026-03-24 21:22:47 +0800

    v2.4: Add significance levels for INP concentration

--- Lanzhou_cfdc/data_process_v2.py
+++ Lanzhou_cfdc/data_process_v2.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # 将全年CFDC数据输出为单个CSV文件
-# 目前版本: v2.3
+# 目前版本: v2.4
 # 版本记录:
 # v2.0: 在v1.0的基础上, 修改了数据处理的思路, 实现: 采样段前后的背景段取平均, 作为这部分采样段的背景值. (如果仅有前背景段, 则取前背景段作为采样段的背景值.)
 # v2.1: 修改了温度和过饱和度的计算方式, 采用平均值而非最后一个值.
 # v2.2, 增加数据质量控制步骤, 去除异常浓度值(包括0值和负值, SS_w<=4或SS_w>=6的值, 2024年9月17日之前未经验证的值)等; 增加"活化温度"列.
 # v2.3, 增加"总采样体积"列, 对INP浓度进行初步的显著性检验(剔除INP浓度小于0.1 #/L的值).
-# 未来版本计划: v2.4, 增加对INP浓度的显著性检验(Schill et al., 2016; DeMott et al., 2017).
+# v2.4, 增加对 INP 浓度的显著性检验(Schill et al., 2016; DeMott et al., 2017): 1. 根据泊松分布计算采样段和背景段的 INP 浓度的标准差; 2. 两标准差的平方和作为 INP 净浓度的误差; 3. 大于 INP 净浓度误差的 1.64 倍才认为是显著的数据点(Z statistic at 95% confidence).
 
 import pandas as pd
 import numpy as np
@@ -92,6 +92,38 @@ def mean_by_consecutive_index(s: pd.Series) -> pd.Series:
     #out.index.name = s.index.name  # 可选：保留索引名
     return out
 
+def process_different_length(sam: pd.Series, bac: pd.Series) -> tuple[pd.Series, pd.Series]:
+    """
+    输入: 两个长度不同的 Series 对象(索引为整数), 每个 Series 的值都属于同一类别(采样段或背景段).
+
+    返回: 两个长度相同的 Series 对象(索引为整数), 处理方式: 删除连续两段(同属采样段或背景段)的前一段.
+
+    调用此函数前, 需要:
+    - import pandas as pd
+    """
+    df1 = pd.DataFrame({'value': sam, 'tag': 'Sample'})
+    df2 = pd.DataFrame({'value': bac, 'tag': 'Background'})
+    df = pd.concat([df1, df2]).sort_index()
+    #### 调试输出, 用于验证删除是否正确
+    #print("Before processing:")
+    #print(df)
+    #### 删除连续两段数据的前一段 (即, 若连续两段数据属于同一tag, 保留后一段)
+    df = df.drop(df[df['tag'] == df['tag'].shift(-1)].index)
+    sam = df[df['tag'] == 'Sample']['value']
+    bac = df[df['tag'] == 'Background']['value']
+
+    ### 对于两种特殊情况的处理: 背景在最后; 样品在最前
+    if df['tag'].iloc[-1]=='Background':
+        bac = bac[:-1]
+    if df['tag'].iloc[0]=='Sample':
+        sam = sam[1:]
+    #### 验证确已删除
+    #print("After processing:")
+    #print("Background: \n", bac)
+    #print("Sample: \n", sam)
+
+    return sam, bac
+
 def compute_net(sam, bac):
     """
     输入: 两个 Series 对象(索引为整数), 计算净值 (Sample - Background), 如果净值小于 0, 则取 0.
@@ -106,35 +138,39 @@ def compute_net(sam, bac):
     if len(sam) != len(bac):
         #### 调试输出
         #print("Sample and background have different number of measurement segments.")
-        
-        ### 如果长度不一致, 尝试进行处理: 
-        df1 = pd.DataFrame({'value': sam, 'tag': 'Sample'})
-        df2 = pd.DataFrame({'value': bac, 'tag': 'Background'})
-        df = pd.concat([df1, df2]).sort_index()
-        #### 调试输出, 用于验证删除是否正确
-        #print("Before processing:")
-        #print(df)
-        #### 删除连续两段数据的前一段 (即, 若连续两段数据属于同一tag, 保留后一段)
-        df = df.drop(df[df['tag'] == df['tag'].shift(-1)].index)
-        sam = df[df['tag'] == 'Sample']['value']
-        bac = df[df['tag'] == 'Background']['value']
 
-        ### 对于两种特殊情况的处理: 背景在最后; 样品在最前
-        if df['tag'].iloc[-1]=='Background':
-            bac = bac[:-1]
-        if df['tag'].iloc[0]=='Sample':
-            sam = sam[1:]
-        ### 验证确已删除
-        #print("After processing:")
-        #print("Background: \n", bac)
-        #print("Sample: \n", sam)
+        sam, bac = process_different_length(sam, bac)
 
         if len(sam) != len(bac):
             raise ValueError("After processing, Sample and Background still have different number of measurement segments.")
     
     net = sam.values - bac.values
-    net[net < 0] = 0
+    #net[net < 0] = 0
     return pd.Series(net, index=sam.index)
+
+def compute_sigma_net(sigma_sam, sigma_bac):
+    """
+    输入: 两个 Series 对象(索引为整数), 分别为采样段和背景段的标准差, 计算净浓度的标准差 (sqrt(sigma_sam^2 + sigma_bac^2)).
+
+    返回: Series 对象, 索引为采样(sample) Series 的索引.
+
+    调用此函数前, 需要:
+    - import pandas as pd
+    """
+    
+    ### 保证两个 Series 长度相同
+    if len(sigma_sam) != len(sigma_bac):
+        #### 调试输出
+        #print("Sample and background have different number of measurement segments.")
+
+        sigma_sam, sigma_bac = process_different_length(sigma_sam, sigma_bac)
+
+        if len(sigma_sam) != len(sigma_bac):
+            raise ValueError("After processing, Sample and Background still have different number of measurement segments.")
+        
+    sigma_net = np.sqrt(sigma_sam.values**2 + sigma_bac.values**2)
+
+    return pd.Series(sigma_net, index=sigma_sam.index)
 
 def main():
     all_dates = []
@@ -143,6 +179,8 @@ def main():
     all_SS_w = []
     all_SS_i = []
     all_vol = []
+    all_sig_level = []
+    all_significant = []
     for temp in TEMPERATURE:
         ## 可视化数据的提取与处理
         ### 处理INP数据
@@ -191,6 +229,12 @@ def main():
             SS_i = mean_by_consecutive_index(SS_i)
             #### 计算净INP浓度
             N_inp_avg_net = compute_net(sam=conc_inp_sam, bac=conc_inp_bac)
+            #### 计算显著性水平
+            sigma_inp_bac = np.sqrt(conc_inp_bac / vol_bac) # 根据泊松分布, INP 浓度的标准差为 sqrt(计数) / 体积 = sqrt(浓度 * 体积) / 体积 = sqrt(浓度 / 体积)
+            sigma_inp_sam = np.sqrt(conc_inp_sam / vol_sam) 
+            sigma_inp_net = compute_sigma_net(sigma_sam=sigma_inp_sam, sigma_bac=sigma_inp_bac)
+            sig_level = 1.64 * sigma_inp_net
+            is_significant = N_inp_avg_net > sig_level
             #### 保存为列表格式
             all_dates.append(time[N_inp_avg_net.index])
             all_INP.append(N_inp_avg_net)
@@ -198,6 +242,8 @@ def main():
             all_SS_w.append(SS_w[N_inp_avg_net.index])
             all_SS_i.append(SS_i[N_inp_avg_net.index])
             all_vol.append(vol_sam[N_inp_avg_net.index])
+            all_sig_level.append(sig_level[N_inp_avg_net.index])
+            all_significant.append(is_significant[N_inp_avg_net.index])
             #### 输出文件名以及该文件处理后的INP数据点数量
             #print(f"Processed file: {file.name}, INP_net count: {len(N_inp_avg_net)}")
             if len(N_inp_avg_net) != 0:
@@ -210,12 +256,16 @@ def main():
     all_SS_w = pd.concat(all_SS_w, ignore_index=True)
     all_SS_i = pd.concat(all_SS_i, ignore_index=True)
     all_vol = pd.concat(all_vol, ignore_index=True)
+    all_sig_level = pd.concat(all_sig_level, ignore_index=True)
+    all_significant = pd.concat(all_significant, ignore_index=True)
     df_inp = pd.DataFrame({'Date': all_dates,
         'N_inp_net(#/L)': all_INP,
         'T_inp(°C)': all_T
         ,'SS_w': all_SS_w
         ,'SS_i': all_SS_i
-        ,'Total_Sampling_Volume(L)': all_vol})
+        ,'Total_Sampling_Volume(L)': all_vol
+        ,'Significance_Level(#/L)': all_sig_level
+        ,'Is_Significant': all_significant})
     df_inp = df_inp.sort_values('Date').reset_index(drop=True)
 
     # 质量控制
@@ -233,9 +283,8 @@ def main():
     df_inp = df_inp[(df_inp['SS_w'] > 4) & (df_inp['SS_w'] < 6)]
     print(f"过饱和度控制后, 点的数量: {df_inp.shape[0]}")
 
-    # 4. 显著性检验: (仅占位, 后续版本补充)
-    df_inp = df_inp[df_inp['N_inp_net(#/L)'] > 0.1]
-    print(f"显著性检验后, 点的数量: {df_inp.shape[0]}")
+    # 4. 显著性检验:
+    print(f"显著性检验后, 点的数量: {df_inp[df_inp['Is_Significant']].shape[0]} (占比: {df_inp[df_inp['Is_Significant']].shape[0]/df_inp.shape[0]:.2%})")
 
     # 5. 根据日期归属, 增加“季节”列
     def season_of_month(m):
@@ -269,7 +318,7 @@ def main():
     df_inp['T_a(°C)'] = df_inp['T_inp(°C)'].apply(activation_temperature)
 
     # 输出为CSV文件
-    df_inp.to_csv(r"D:\Coding\Data\Lanzhou_cfdc\processed\N_INP(202409-202509)v2.3.csv", index=False)
+    df_inp.to_csv(r"D:\Coding\Data\Lanzhou_cfdc\processed\N_INP(202409-202509)v2.4.csv", index=False)
 
 
 if __name__ == "__main__":
